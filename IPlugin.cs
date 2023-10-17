3bbb35b6e124c3be257d5dbcb83c1745b2c28d5e
Vladimir <79203161+Primus-max@users.noreply.github.com>
2023-10-17 19:28:28 +0800

    Документирование IPlugin

--- IPlugin.cs
+++ IPlugin.cs
@@ -1,11 +1,30 @@
-﻿namespace ds.test.impl
+﻿using System.Drawing;
+
+namespace ds.test.impl
 {
+    /// <summary>
+    /// Общий интерфейс плагина
+    /// </summary>
     public interface IPlugin
     {
+        /// <summary>Имя плагина</summary>
         string PluginName { get; }
+
+        /// <summary>Версия плагина</summary>
         string Version { get; }
-        System.Drawing.Image Image { get; }
+
+        /// <summary>Изображение плагина</summary>
+        Image Image { get; }
+
+        /// <summary>Описание плагина</summary>
         string Description { get; }
+
+        /// <summary>
+        /// Метод (точка входа) выполняет математическую операцию над двумя целыми числами.
+        /// </summary>
+        /// <param name="input1">Первый операнд, целое число</param>
+        /// <param name="input2">Второй операнд, целое число</param>
+        /// <returns>Результат операции над входными числами.</returns>
         int Run(int input1, int input2);
     }
 }
--- Plugins.cs
+++ Plugins.cs
@@ -1,7 +1,32 @@
 ﻿namespace ds.test.impl
 {
-    public class Plugins
+    public static class Plugins
     {
+        private static IPlugin[] plugins = { new AdditionPlugin(), new MultiplicationPlugin() };
 
+        public static int PluginsCount => plugins.Length;
+
+        public static string[] GetPluginNames()
+        {
+            string[] names = new string[PluginsCount];
+            for (int i = 0; i < PluginsCount; i++)
+            {
+                names[i] = plugins[i].PluginName;
+            }
+            return names;
+        }
+
+        public static IPlugin GetPlugin(string pluginName)
+        {
+            foreach (var plugin in plugins)
+            {
+                if (plugin.PluginName == pluginName)
+                {
+                    return plugin;
+                }
+            }
+            return null;
+        }
     }
+
 }
